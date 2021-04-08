7536d428753ed6c164892d89c1ca640d7a50e2a4
ihor <ihor.v.savchenko@gmail.com>
2021-04-09 02:21:51 +0300

    slight change

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '871052573:AAFgzi_GaDOz-2m7I6H0aN8uY27XAQYyUrc'  # bot token from @BotFather
+TOKEN = 'your specific digital token'  # bot token from @BotFather
 # store files in /tmp so storage does not get complete
 result_storage_path = 'tmp'
 dict_styles = {'jack': 4098, 'sape': 4139}
